3cdc38756f9556a842db4916344e66c0f9a70a58
Preston Fassbender <fassbenderp0551@gmail.com>
2022-02-16 21:52:40 -0600

    delete function working correctly with addition of readfilesync

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,36 +1,32 @@
 const router = require('express').Router();
-const { notesArray } = require('../../db/db');
-const { createNewNote, updateDB } = require("../../lib/notes");
+let notesArray = require('../../db/db');
+const fs = require("fs");
+const path = require("path");
+const { createNewNote } = require("../../lib/notes");
 
 router.get("/notes", (req, res) => {
+    notesArray = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
     res.json(notesArray);
 });
 
 router.post("/notes", (req, res) => {
-    req.body.id = notesArray.length.toString()
+    req.body.id = Math.floor(Math.random() * 1000)
     const note = createNewNote(req.body, notesArray);
     res.json(note);
 });
 
 router.delete('/notes/:id', (req, res) => {
-    // let newNotesArray = notesArray.filter(
-    //     (note) => { return note.id != req.params.id; }
-    // );
-    // //console.log(newNotesArray);
-    // res.json(newNotesArray);
-    
-    // res.json(notesArray.filter(note => note.id != req.params.id));
-    // returns an updated array with the "deleted" note id filtered out
-    // however this doesn't do anything when I click the delete button
-    // am I supposed to writeFile again?
     let newArray = [];
     newArray = notesArray.filter(note => note.id != req.params.id)
-    updateDB(newArray);
-    console.log("notesRoutes: " + newArray)
-    res.json(newArray);
-    
-    // this updates the db file to have a new array with the deleted note gone
-    // it doesn't adjust the page to show the note is gone
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify(newArray), (err, res) => {
+            if (err) {
+                throw err
+            }
+        }
+    )
+    res.json("delete successful")
 })
 
 module.exports = router;
